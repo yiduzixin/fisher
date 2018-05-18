49de7dbfb5a8d688e9767c9a49636937ca41de2c
yiduzixin <yiduzixin@163.com>
2018-05-19 00:26:24 +0800

    yushu
    鱼书

---
+++ app/secure.py
@@ -0,0 +1,7 @@
+"""
+Created by 逗蛆 on 2018/5/6
+
+"""
+__author__ = "逗蛆"
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@localhost:3306/fisher'
---
+++ app/web/book.py
@@ -0,0 +1,32 @@
+"""
+Created by 逗蛆 on 2018/5/7
+
+"""
+from flask import jsonify, request
+
+from app.forms.book import SearchForm
+from app.libs.helper import is_isbn_or_key
+from app.spider.yushu_book import YuShuBook
+from app.view_models.book import BookCollection
+from app.web import web
+
+__author__ = "逗蛆"
+
+
+@web.route('/book/search')
+def search():
+    form = SearchForm(request.args)
+    books = BookCollection()
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
+        if isbn_or_key == 'isbn':
+            yushu_book.search_by_isbn(q)
+        else:
+            yushu_book.search_by_keyword(q)
+        books.fill(yushu_book, q)
+        return jsonify(books)
+    else:
+        return jsonify(form.errors)
